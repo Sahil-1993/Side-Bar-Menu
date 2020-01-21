0a70d0aa6a226b10d7351d1d6b161e7ae1290bbf
Sahil-1993 <55412573+Sahil-1993@users.noreply.github.com>
2020-01-20 20:02:56 -0500

    Initial Commit

---
+++ Side Bar Menu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Side Bar Menu
+//
+//  Created by Sahil Arora on 2020-01-20.
+//  Copyright © 2020 Sahil Arora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Side Bar MenuTests/Side_Bar_MenuTests.swift
@@ -0,0 +1,34 @@
+//
+//  Side_Bar_MenuTests.swift
+//  Side Bar MenuTests
+//
+//  Created by Sahil Arora on 2020-01-20.
+//  Copyright © 2020 Sahil Arora. All rights reserved.
+//
+
+import XCTest
+@testable import Side_Bar_Menu
+
+class Side_Bar_MenuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
